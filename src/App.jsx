288cc27fb5bfe99a0b2f7676b37140748754f71a
SALAB\dellv <salabyaham@gmail.com>
2024-01-04 16:12:05 +0600

    Outlet has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './SharedComponent/Navbar'
 
 function App() {
   
 
   return (
    <div>
-    
+    <Navbar></Navbar>
+    <Outlet></Outlet>
    </div>
   )
 }

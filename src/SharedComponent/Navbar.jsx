5639de6dbbc149ce6978c33d3ac5778ae261c978
SALAB\dellv <salabyaham@gmail.com>
2024-01-07 00:47:57 +0600

    fixed - navbar position has been fixed with z index

--- src/SharedComponent/Navbar.jsx
+++ src/SharedComponent/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
     </>
     return (
-        <div className="fixed top-0 w-full" >
+        <div className="fixed top-0 z-10 w-full" >
             <div className="navbar flex-col md:flex-row lg:flex-row bg-neutral text-neutral-content">
                 <Link to="/"><button className="btn btn-ghost text-xl text-yellow-600 font-sans font-semibold">Send Easy</button></Link>
                 <div className="text-white font-extrabold flex mx-auto"><IoCallSharp className="mr-2" /> Call us  :   <span className="text-yellow-600 ml-2">  +8801701474332</span></div>

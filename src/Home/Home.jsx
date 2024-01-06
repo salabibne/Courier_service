fd7abf66cb6e8132c87c2cb9b659cc53885eaf9c
SALAB\dellv <salabyaham@gmail.com>
2024-01-07 00:48:48 +0600

    new componnet everything hasbeen added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../SharedComponent/About/About";
 import Bannar from "./Bannars/Bannar";
+import Everything from "./Everything";
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
             <About></About>
+            <Everything></Everything>
         </div>
     );
 };

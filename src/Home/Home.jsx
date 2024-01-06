b3b52d2ab3a76c5fd1ba21facb4bac3ecd0d1249
SALAB\dellv <salabyaham@gmail.com>
2024-01-06 11:55:26 +0600

    about component has been imported

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,11 @@
+import About from "../SharedComponent/About/About";
 import Bannar from "./Bannars/Bannar";
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
+            <About></About>
         </div>
     );
 };

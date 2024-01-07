5366b55756b0650beaf3bf9e71af2648a994e00a
SALAB\dellv <salabyaham@gmail.com>
2024-01-07 21:25:59 +0600

    customer feedback component section has   been added  in homne componnet

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,7 @@
 import About from "../SharedComponent/About/About";
 import Bannar from "./Bannars/Bannar";
 import Everything from "./Everything";
+import Feedback from "./Feedback";
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Bannar></Bannar>
             <About></About>
             <Everything></Everything>
+            <Feedback></Feedback>
         </div>
     );
 };
